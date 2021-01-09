6a27b33f95d22becb0815b485f0005ac0e7b0a2a
valentinapescuma <valentina.pescuma@gmail.com>
2021-01-09 15:16:40 +0100

    correct number-invariant noun filter

---
+++ main_script_v2.1.R
@@ -0,0 +1,161 @@
+################################################################
+# ITALIAN INFLECTIONAL MORPHPOPHONOLOGY ########################
+# PESCUMA FRANZON ZANINI #######################################
+# v 2.1 09 JAN 2021 ############################################
+################################################################
+
+# clear environment
+
+rm(list=ls())
+
+#codewd
+#codewd=("C:\\Users\\FF\\Documents\\Analisi varie\\Italian Morphophonology")
+
+# input directory
+#inwd <- '/Users/valentinapescuma/Documents/PhD/ItalianMorphophonolgy'
+inwd=("/Users/valentinapescuma/Documents/PhD/ItalianMorphophonology")
+# output directory 
+#outwd=
+
+setwd(inwd)
+
+# ### #
+#
+#   IMPORT AND PREPROCESS
+#
+
+# import complete ITWAC list of nouns ####
+itwac_all <- read.csv('itwac_nouns_lemmas_notail_unique_utf8.csv', header=T, sep=',', encoding='UTF-8')
+
+summary(itwac_all)
+head(itwac_all, 20)
+itwac_all$Freq <- as.integer(itwac_all$Freq)
+
+# import morphit####
+morphit <- read.delim('morph-it_048.txt', sep='\t', header = F, encoding = 'UTF-8')
+
+colnames(morphit) <- c('form', 'lemma', 'POS')
+morphit$form <- as.character(morphit$form)
+
+table(morphit$POS)
+
+# select nouns in morphit####
+nouns_morphit <- morphit[morphit$POS %in% c('NOUN-F:s','NOUN-F:p','NOUN-M:s', 'NOUN-M:p'),]
+nouns_morphit$POS <- factor(nouns_morphit$POS)
+summary(nouns_morphit) #misura perdita occorrenze
+
+# merge morphit and itwac nouns ####
+nouns_all <- merge(nouns_morphit, itwac_all, by.x='form', by.y='Form') #33976
+nouns_all <- nouns_all[,c(1,3,4,5)]
+
+summary(nouns_all)
+
+
+# a column with Gender value
+nouns_all$Gender=ifelse(grepl("NOUN-M:",nouns_all$POS.x),  "masc", "femm")
+checkgender= ifelse(grepl("NOUN-F:",nouns_all$POS.x),  "femm", "masc")
+summary(checkgender==nouns_all$Gender)
+nouns_all$Gender=as.factor(nouns_all$Gender)
+
+# a column with Number value
+nouns_all$Number=ifelse(grepl(":p",nouns_all$POS.x),  "plur", "sing")
+checknumber= ifelse(grepl(":s",nouns_all$POS.x),  "sing", "plur")
+summary(checknumber==nouns_all$Number)
+nouns_all$Number=as.factor(nouns_all$Number)
+#nouns_all$Number=as.factor(nouns_all$Number)
+
+
+# output csv, that can work as a lighter input for what comes from now on
+#write.csv(nouns_all, )
+
+# use csv as input and start from here
+
+
+# ### #
+#
+#   AMBIGUOUS FORM - FEATURE MAPPING 
+#
+
+#   How many noun forms (type and token) cannot be unambiguously related to one inflectional feature?
+
+## Possible ambiguity:
+
+# (1) ambiguous across lemmas "Lem_Amb" = (e.g., signore MS and FP; fine MS and FS) pairwise comparison: same form, different lemma 
+#     more than one feature surfaces in the same form, but lemmas are different 
+#     a. two lemmas: one with a |, for example cameriere|cameriera; the other one is cameriere, but we keep the form of the latter.
+#     b. invariants for gender  but two distinct forms between numbers (abitante, abitanti). Lemma>=4, form>=2, Gender>=2, Number>=2
+
+# (2) invariant (number) "Num_Inv" = (e.g., città ) pairwise comparison: same form, same lemma, same gender, different number 
+
+# (3) invariant (gender) "Gen_Inv" = (e.g., musicista ) pairwise comparison: same form in the singular number, same lemma, different gender. Three forms for one lemma expected.
+
+# (4) invariant (gender+number) "Full_Inv" = (e.g., capolista) pairwise comparison: same form, same lemma, different gender, different number 
+#type "4" is mostly represented by compounds or loanwords denoting animates "apripista" "leader", "manager".
+#(4b) is a subset of 4: mostly compounds that can be inflected or invariant "capogruppo"MP|"capigruppo"MP
+
+
+# subset to try out the operations. comment when ready
+nouns_allZ= nouns_all
+
+# (1) Lem_Amb
+# a. two lemmas: one with a |, for example cameriere|cameriera
+# some are already tagged 
+nouns_allY <- nouns_allZ
+
+Lem_Amb_1_all=nouns_allY[grep ("\\|", nouns_allY$Lemma), ]
+
+head(Lem_Amb_1_all$form,50)
+
+# subtract Lem_amb from nouns_allY, and obtain a new df nouns_allY
+nouns_allY= nouns_allY[!(nouns_allY$form %in% Lem_Amb_1_all$form), ]
+
+# nouns_allY$form=as.factor(nouns_allY$form)
+summary(nouns_allY)
+
+# # b. invariants for gender  but two distinct forms between numbers (abitante, abitanti). Lemma>=4, form>=2, Gender>=2, Number>=2
+# same form, different lemma --> number of lemmas > number of forms & number of POS >= number of forms
+Lem_Amb_2_all= nouns_allY[nouns_allY$Lemma %in% names(which(table(nouns_allY$Lemma) >= 4)) &
+                        nouns_allY$form %in% names(which(table(nouns_allY$form) >= 2)) &
+                        nouns_allY$Gender %in% names(which(table(nouns_allY$Gender) >= 2)) &
+                        nouns_allY$Number %in% names(which(table(nouns_allY$Number) >= 2))  ,]
+
+
+nouns_allY= nouns_allY[!(nouns_allY$form %in% Lem_Amb_2_all$form), ]
+
+# (2) invariant (number) "Num_Inv" = (e.g., città ) pairwise comparison: same form, same lemma, same gender, different number 
+
+Num_Inv_all = nouns_allY[ nouns_allY$Lemma %in% names(which(table(nouns_allY$Lemma) >= 2)) &
+                               nouns_allY$form %in% names(which(table(nouns_allY$form) >= 2)) &
+                               nouns_allY$Gender %in% names(which(table(nouns_allY$Gender) >= 2)) &
+                               nouns_allY$Number %in% names(which(table(nouns_allY$Number) >= 1)),  ]
+
+nouns_allX= nouns_allY[!(nouns_allY$form %in% Num_Inv_all$form), ]
+1-nrow(nouns_allX)/nrow(nouns_allZ) # data loss from original merged dataset = 11.37% 
+
+### NOTE: with Num_Inv_all we have already deleted all 16 items that we couldn't delete when splitting 
+#into fem and masc subset (see below, now commented out), 
+#which means that all the gender invariant nouns seem to have been taken care of by code above.
+ 
+# # subsets with fem and masc nouns only
+# # NB: this doesn't work well, as some nouns remain excluded 
+# fem_nouns <- nouns_allY[nouns_allY$Gender == 'femm',]
+# masc_nouns <- nouns_allY[nouns_allY$Gender == 'masc',]
+# 
+# Num_Inv_fem = fem_nouns[ fem_nouns$Lemma %in% names(which(table(fem_nouns$Lemma) >= 2)) &
+#                             fem_nouns$form %in% names(which(table(fem_nouns$form) >= 2)) &
+#                             fem_nouns$Number %in% names(which(table(fem_nouns$Number) >= 1)),  ]
+# 
+# Num_Inv_masc = masc_nouns[ masc_nouns$Lemma %in% names(which(table(masc_nouns$Lemma) >= 2)) &
+#                            masc_nouns$form %in% names(which(table(masc_nouns$form) >= 2)) &
+#                            masc_nouns$Number %in% names(which(table(masc_nouns$Number) >= 1)),  ]
+# 
+# All_Inv <- rbind(Num_Inv_fem, Num_Inv_masc)
+# 
+# nouns_allNew <- nouns_allY[!(nouns_allY$form %in% All_Inv$form), ]
+# 
+# All_Inv_2_sub <- All_Inv_2[All_Inv_2$form %in% names(which(table(All_Inv_2$form)==3)),]
+# 
+# setdiff(Num_Inv_all$form, All_Inv$form)
+# 
+# test <- Num_Inv_all[!(Num_Inv_all$form %in% All_Inv$form), ]
+
